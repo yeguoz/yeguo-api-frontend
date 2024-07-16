366e72693102d1ee00f5da70233167ffd8831687
ye-guo <aidjajd@163.com>
2024-07-16 11:12:24 +0800

    feat: 增加果币商城页面

--- src/pages/CoinMall/components/CoinMallCard/index.tsx
+++ src/pages/CoinMall/components/CoinMallCard/index.tsx
@@ -1,15 +1,33 @@
+import { useRef } from 'react';
 import styles from './index.css';
-export default () => {
+
+const CoinMallCard = ({
+  CNY,
+  goldCoin,
+  isSelected,
+  onClick,
+}: {
+  CNY: number;
+  goldCoin: number;
+  isSelected: boolean;
+  onClick: (cardPriceRef: any) => void;
+}) => {
+  const cardPriceRef = useRef<HTMLSpanElement>(null);
+
   return (
-    <div className={styles.card}>
-      {/* 上 */}
+    <div
+      className={`${styles.card} ${isSelected ? styles.active : ''}`}
+      onClick={() => onClick(cardPriceRef)}
+    >
       <div className={styles.top}>
         <div className={styles.info}>
-          <span>100果币</span>
+          <span>{goldCoin}果币</span>
           <img src="/assets/money.png" alt="" className={styles.smallIcon} />
-          <span className={styles.price}>￥ 体验 0.01</span>
+          <span className={styles.price}>
+            ￥<span ref={cardPriceRef}>{CNY}</span>
+          </span>
         </div>
-        <p>增加100果币</p>
+        <p>增加{goldCoin}果币</p>
       </div>
       <div className={styles.bottom}>
         <img src="/assets/moneyC.png" alt="" className={styles.btmImg} />
@@ -17,3 +35,5 @@ export default () => {
     </div>
   );
 };
+
+export default CoinMallCard;
--- src/pages/CoinMall/index.tsx
+++ src/pages/CoinMall/index.tsx
@@ -1,12 +1,42 @@
 import Container from '@/components/Container';
 import { ProCard } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { Col, Row } from 'antd';
+import { Button, Col, Row } from 'antd';
+import { useRef, useState } from 'react';
 import CoinMallCard from './components/CoinMallCard';
 
 export default () => {
   const { initialState } = useModel('@@initialState');
+  const data = [
+    { CNY: 0.09, goldCoin: 100 },
+    { CNY: 0.99, goldCoin: 1000 },
+    { CNY: 1.99, goldCoin: 3000 },
+    { CNY: 6.99, goldCoin: 9999 },
+    { CNY: 9.99, goldCoin: 18999 },
+    { CNY: 19.99, goldCoin: 32999 },
+  ];
 
+  const [selectedCard, setSelectedCard] = useState<number>();
+  const selectedCardPriceRef = useRef<HTMLSpanElement>();
+  const [payment, setPayment] = useState<number>(0);
+
+  const handleCardClick = (index: number, ref: any) => {
+    // 选中高亮
+    setSelectedCard(index);
+    selectedCardPriceRef.current = ref.current;
+    if (selectedCardPriceRef.current) {
+      const priceStr = selectedCardPriceRef.current.textContent;
+      const moneyNum = parseFloat(priceStr!);
+      setPayment(moneyNum);
+    }
+  };
+
+  const handlerBuy = () => {
+    if (payment <= 0) return;
+    // todo 发起订单，跳转支付页面（我已经支付（跳转支付成功页面），取消支付（跳转到订单列表））
+    // 后端需要生成订单，成功后跳转支付页面
+    console.log('购买');
+  };
   return (
     <Container>
       <ProCard
@@ -39,140 +69,56 @@ export default () => {
         style={{ marginTop: '1rem' }}
       >
         <Row gutter={[20, 24]}>
-          <Col
-            xs={{
-              flex: '100%',
-            }}
-            sm={{
-              flex: '50%',
-            }}
-            md={{
-              flex: '40%',
-            }}
-            lg={{
-              flex: '20%',
-            }}
-            xl={{
-              flex: '5%',
-            }}
-          >
-            <CoinMallCard />
-          </Col>
-          <Col
-            xs={{
-              flex: '100%',
-            }}
-            sm={{
-              flex: '50%',
-            }}
-            md={{
-              flex: '40%',
-            }}
-            lg={{
-              flex: '20%',
-            }}
-            xl={{
-              flex: '5%',
-            }}
-          >
-            <CoinMallCard />
-          </Col>
-          <Col
-            xs={{
-              flex: '100%',
-            }}
-            sm={{
-              flex: '50%',
-            }}
-            md={{
-              flex: '40%',
-            }}
-            lg={{
-              flex: '20%',
-            }}
-            xl={{
-              flex: '5%',
-            }}
-          >
-            <CoinMallCard />
-          </Col>
-          <Col
-            xs={{
-              flex: '100%',
-            }}
-            sm={{
-              flex: '50%',
-            }}
-            md={{
-              flex: '40%',
-            }}
-            lg={{
-              flex: '20%',
-            }}
-            xl={{
-              flex: '5%',
-            }}
-          >
-            <CoinMallCard />
-          </Col>
-          <Col
-            xs={{
-              flex: '100%',
-            }}
-            sm={{
-              flex: '50%',
-            }}
-            md={{
-              flex: '40%',
-            }}
-            lg={{
-              flex: '20%',
-            }}
-            xl={{
-              flex: '5%',
-            }}
-          >
-            <CoinMallCard />
-          </Col>
-          <Col
-            xs={{
-              flex: '100%',
-            }}
-            sm={{
-              flex: '50%',
-            }}
-            md={{
-              flex: '40%',
-            }}
-            lg={{
-              flex: '20%',
-            }}
-            xl={{
-              flex: '5%',
-            }}
-          >
-            <CoinMallCard />
-          </Col>
-          <Col
-            xs={{
-              flex: '100%',
-            }}
-            sm={{
-              flex: '50%',
-            }}
-            md={{
-              flex: '40%',
-            }}
-            lg={{
-              flex: '20%',
-            }}
-            xl={{
-              flex: '5%',
-            }}
-          >
-            <CoinMallCard />
-          </Col>
+          {data.map((item, index) => (
+            <Col
+              key={index}
+              xs={{
+                flex: '100%',
+              }}
+              sm={{
+                flex: '50%',
+              }}
+              md={{
+                flex: '40%',
+              }}
+              lg={{
+                flex: '20%',
+              }}
+              xl={{
+                flex: '5%',
+              }}
+            >
+              <CoinMallCard
+                CNY={item.CNY}
+                goldCoin={item.goldCoin}
+                isSelected={selectedCard === index}
+                onClick={(cardPriceRef) => handleCardClick(index, cardPriceRef)}
+              />
+            </Col>
+          ))}
         </Row>
+        <p style={{ textAlign: 'center', padding: '1rem 0 0' }}>
+          本商品为虚拟内容，用于平台接口调用，购买后不支持
+          <span style={{ color: '#d23918' }}>退换</span>，购买后24小时内审核完成。
+        </p>
+      </ProCard>
+      <ProCard bordered>
+        <div style={{ display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
+          <span style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>实付</span>
+          <span
+            style={{
+              marginLeft: '0.5rem',
+              marginRight: '1.5rem',
+              color: '#d23918',
+              fontSize: '1.5rem',
+            }}
+          >
+            ￥<span>{payment}</span>
+          </span>
+          <Button type="primary" onClick={handlerBuy}>
+            确认购买
+          </Button>
+        </div>
       </ProCard>
     </Container>
   );
