3dcf683287229908fa45e73ce201a0db7316efcf
yeguoz <aidjajd@163.com>
2024-12-20 00:03:52 +0800

    refactor: MD5算法-->SHA256算法 (更安全)

--- src/pages/utils/generateSignature.ts
+++ src/pages/utils/generateSignature.ts
@@ -3,7 +3,7 @@ import CryptoJS from 'crypto-js';
 const generateSignature = (message: string, secretKey: any) => {
   // method + url + herder 拼接，使用secretKey进行签名
   // 计算 HMAC-MD5 签名
-  const hash = CryptoJS.HmacMD5(message, secretKey);
+  const hash = CryptoJS.SHA256(message, secretKey);
   console.log('hash::' + hash);
   // 将签名转换为字符串输出
   return hash.toString(CryptoJS.enc.Hex);
