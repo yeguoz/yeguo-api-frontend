ce28be43516b075d04b3566633b5ab999dadb4d8
yeguoz <aidjajd@163.com>
2025-02-26 22:49:03 +0800

    chore: Pay page route

--- config/routes.ts
+++ config/routes.ts
@@ -34,7 +34,6 @@ export default [
       },
     ],
   },
-  { name: '支付页', path: '/:orderId/pay', component: './Pay', hideInMenu: true },
   { name: '果币商城', icon: 'PayCircle', path: '/coinmall', component: './CoinMall' },
   { name: '我的订单', icon: 'UnorderedList', path: '/orderinfo', component: './OrderInfo' },
   { name: '个人主页', icon: 'User', path: '/person', component: './Person' },
